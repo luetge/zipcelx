1c8650d8e8813d7cf1f3ae14020c8c0fa40458a1
luetge <katha.lysator@yahoo.de>
2017-04-26 19:19:42 +0200

    Update constants.js

--- src/commons/constants.js
+++ src/commons/constants.js
@@ -1,6 +1,7 @@
 export const CELL_TYPE_STRING = 'string';
 export const CELL_TYPE_NUMBER = 'number';
-export const validTypes = [CELL_TYPE_STRING, CELL_TYPE_NUMBER];
+export const CELL_TYPE_DATETIME = 'datetime';
+export const validTypes = [CELL_TYPE_STRING, CELL_TYPE_NUMBER, CELL_TYPE_DATETIME];
 
 export const MISSING_KEY_FILENAME = 'Zipclex config missing property filename';
 export const INVALID_TYPE_FILENAME = 'Zipclex filename can only be of type string';
